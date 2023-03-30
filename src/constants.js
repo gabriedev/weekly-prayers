ef225e0931a10bc61c6aa941daceb5f99559a7cb
gabriedev <gabriedev@gmail.com>
2023-03-30 15:46:51 +0000

    Added Reels from Instagram.

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,4 @@
-export const { PAGE_URI } = process.env
+export const { PAGE_URI, INSTAGRAM_API_KEY } = process.env
+export const USER_ID = 32502842659
+export const NUMBER_OF_REELS = 50
+export const INSTAGRAM_API_URL = 'instagram28.p.rapidapi.com'
--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
 import { resolve } from 'path'
-import { PAGE_URI } from './constants.js'
+import {
+  INSTAGRAM_API_KEY,
+  INSTAGRAM_API_URL,
+  NUMBER_OF_REELS,
+  PAGE_URI,
+  USER_ID
+} from './constants.js'
 import { promises as fs } from 'fs'
-import { chromium } from 'playwright'
 import { selector } from './utils.js'
-;(async () => {
+import { chromium } from 'playwright'
+
+function mapReelFromInstagramReels({ id, display_resources, video_url }) {
+  return {
+    id,
+    pictures: display_resources,
+    url: video_url
+  }
+}
+
+async function getReelsFromInstagramApi() {
+  const response = await fetch(
+    `https://${INSTAGRAM_API_URL}/medias?user_id=${USER_ID}&batch_size=${NUMBER_OF_REELS}`,
+    {
+      headers: {
+        'X-RapidAPI-Key': INSTAGRAM_API_KEY,
+        'X-RapidAPI-Host': INSTAGRAM_API_URL
+      }
+    }
+  )
+  const { data } = await response.json()
+
+  return data.user.edge_owner_to_timeline_media.edges.map(
+    mapReelFromInstagramReels
+  )
+}
+
+async function getDailyWord() {
   const browser = await chromium.launch()
   const page = await browser.newPage()
   await page.goto(PAGE_URI)
+
   const [date, readingOfToday, gospelOfTheDay] = await Promise.all([
     page.textContent('#dataFilter-text'),
     {
@@ -23,8 +56,17 @@ import { selector } from './utils.js'
 
   await browser.close()
 
-  await fs.writeFile(
-    resolve('daily.json'),
-    JSON.stringify({ date, readingOfToday, gospelOfTheDay })
-  )
+  return { date, readingOfToday, gospelOfTheDay }
+}
+
+;(async () => {
+  const [dailyWord, reels] = await Promise.all([
+    getDailyWord(),
+    getReelsFromInstagramApi()
+  ])
+
+  await Promise.all([
+    await fs.writeFile(resolve('daily.json'), JSON.stringify(dailyWord)),
+    await fs.writeFile(resolve('reels.json'), JSON.stringify(reels))
+  ])
 })()
