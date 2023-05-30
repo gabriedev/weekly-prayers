ec27e3ca1cd11fc2ca1f5c69153567181b19533b
gabriedev <gabrielitodev@gmail.com>
2023-05-29 23:03:57 -0400

    Updated repo

--- src/constants.js
+++
@@ -1,4 +0,0 @@
-export const { PAGE_URI, INSTAGRAM_API_KEY } = process.env
-export const USER_ID = 32502842659
-export const NUMBER_OF_REELS = 50
-export const INSTAGRAM_API_URL = 'instagram28.p.rapidapi.com'
--- src/index.js
+++ src/index.js
@@ -1,74 +1,33 @@
 import { resolve } from 'path'
-import {
-  INSTAGRAM_API_KEY,
-  INSTAGRAM_API_URL,
-  NUMBER_OF_REELS,
-  PAGE_URI,
-  USER_ID
-} from './constants.js'
 import { promises as fs } from 'fs'
 import { selector } from './utils.js'
 import { chromium } from 'playwright'
 
-async function mapReelFromInstagramReels({ node }, index) {
-  const { id, display_url, video_url } = node
+const { PAGE_URI } = process.env
 
+async function pageTextContext(n) {
   return {
-    id,
-    pictures: display_url,
-    url: video_url
+    title: (await page.textContent(selector(n, 1))).trim(),
+    book: (await page.textContent(selector(n, 2))).trim(),
+    content: (await page.textContent(selector(n, 3))).trim()
   }
 }
 
-async function getReelsFromInstagramApi() {
-  const response = await fetch(
-    `https://${INSTAGRAM_API_URL}/medias?user_id=${USER_ID}&batch_size=${NUMBER_OF_REELS}`,
-    {
-      headers: {
-        'X-RapidAPI-Key': INSTAGRAM_API_KEY,
-        'X-RapidAPI-Host': INSTAGRAM_API_URL
-      }
-    }
-  )
-  const { data } = await response.json()
-
-  return await Promise.all(
-    data.user.edge_owner_to_timeline_media.edges.map(mapReelFromInstagramReels)
-  )
-}
-
-async function getDailyWord() {
+;(async () => {
   const browser = await chromium.launch()
   const page = await browser.newPage()
   await page.goto(PAGE_URI)
 
   const [date, readingOfToday, gospelOfTheDay] = await Promise.all([
     page.textContent('#dataFilter-text'),
-    {
-      title: await page.textContent(selector(3, 1)),
-      book: await page.textContent(selector(3, 2)),
-      content: await page.textContent(selector(3, 3))
-    },
-    {
-      title: await page.textContent(selector(4, 1)),
-      book: await page.textContent(selector(4, 2)),
-      content: await page.textContent(selector(4, 3))
-    }
+    pageTextContext(3),
+    pageTextContext(4)
   ])
 
   await browser.close()
 
-  return { date, readingOfToday, gospelOfTheDay }
-}
-
-;(async () => {
-  const [dailyWord, reels] = await Promise.all([
-    getDailyWord(),
-    getReelsFromInstagramApi()
-  ])
-
-  await Promise.all([
-    await fs.writeFile(resolve('daily.json'), JSON.stringify(dailyWord)),
-    await fs.writeFile(resolve('reels.json'), JSON.stringify(reels))
-  ])
+  await fs.writeFile(
+    resolve('word.json'),
+    JSON.stringify({ date, readingOfToday, gospelOfTheDay })
+  )
 })()
